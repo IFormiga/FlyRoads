5361e99767dc52214075419fcd82b85f6300556e
Nycolas <Nycolas@Nycolas>
2016-06-16 19:55:37 -0300

    Testando

--- FlyWings/src/dados/RepositorioHoteis.java
+++ FlyWings/src/dados/RepositorioHoteis.java
@@ -13,8 +13,8 @@ import negocio.Hotel;
 public class RepositorioHoteis implements IRepositorioHoteis {
 
 private List<Hotel> listaHoteis = new ArrayList<Hotel>();
-	
-	
+
+
 	public void salvarHotel(Hotel hotel1)throws HEException
 	{
 	   	if(listaHoteis.contains(hotel1))
@@ -24,13 +24,13 @@ private List<Hotel> listaHoteis = new ArrayList<Hotel>();
 	   	}
 	   	else
 	   	{
-	   		listaHoteis.add(hotel1);
+	   		this.listaHoteis.add(hotel1);
 	   	}
 	}
-	
+
 	public void deletarHotel(Hotel hotel1)throws HNException
 	{
-		
+
 	    if(listaHoteis.contains(hotel1))
 			{
 				listaHoteis.remove(hotel1);
@@ -41,27 +41,27 @@ private List<Hotel> listaHoteis = new ArrayList<Hotel>();
 			throw hne;
 		}
 	}
-	
+
 	public Hotel procurarHotel(Hotel hotel1)throws HNException
 	{
 		if(listaHoteis.contains(hotel1))
 		{
 			return hotel1;
-			
+
 		}
 		else
 		{
 			throw new HNException(hotel1.getEndereço(),hotel1.getCEP(),hotel1.getNome_Hotel());
 		}
 	}
-	
+
 	public boolean atualizarHotel(Hotel hotel_para_alt, Hotel hotel_alt)throws HNException, HEException
 	{
 		boolean r = false;
 		if(listaHoteis.contains(hotel_para_alt))
 		{
 			listaHoteis.remove(hotel_para_alt);
-			
+
 			if(!listaHoteis.contains(hotel_alt))
 			{
 				listaHoteis.add(hotel_alt);
@@ -72,23 +72,23 @@ private List<Hotel> listaHoteis = new ArrayList<Hotel>();
 				 HEException hee = new HEException(hotel_alt.getEndereço(),hotel_alt.getCEP(),hotel_alt.getNome_Hotel());
 				  throw hee;
 			}
-			
+
 		}
 		else
 		{
 			HNException hne = new HNException(hotel_para_alt.getEndereço(),hotel_para_alt.getCEP(),hotel_para_alt.getNome_Hotel());
 			throw hne;
 		}
-		
+
 		return r;
 	}
 
-	
+
 	public List<Hotel> listarHoteis() {
 		return Collections.unmodifiableList(this.listaHoteis);
 	}
 
-	
+
 	public boolean existe(Hotel hotel) {
 		boolean resultado = false;
 		for (Hotel hotel2 : listaHoteis) {
@@ -97,15 +97,15 @@ private List<Hotel> listaHoteis = new ArrayList<Hotel>();
 				{
 					resultado = true;
 				}
-		    	
+
 		    }
-		     
+
 		}
 		return resultado;
 	}
-	
 
-	
+
+
 	public boolean remover(Hotel hotel) {
 		boolean resultado = false;
 		for (Hotel hotel2 : listaHoteis) {
@@ -116,6 +116,6 @@ private List<Hotel> listaHoteis = new ArrayList<Hotel>();
 			}
 		}
 		return resultado;
-	} 
-	
+	}
+
 }
